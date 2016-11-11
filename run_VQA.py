c0a69c772d145ddaf334bf7a5b9a0175745f8d83
AaronLai <aaronlaiya@icloud.com>
2016-11-12 00:59:55 +0800

    add clean data

--- run_VQA.py
+++ run_VQA.py
@@ -2,82 +2,33 @@
 # @Author: aaronlai
 # @Date:   2016-11-11 16:45:09
 # @Last Modified by:   AaronLai
-# @Last Modified time: 2016-11-11 18:36:15
+# @Last Modified time: 2016-11-12 00:59:35
 
 import pandas as pd
 import numpy as np
 import json
 import csv
+import re
 
 from collections import defaultdict
-from utils import judge
-
+from utils import load_train_data, clean_data
+from datetime import datetime
 
 base_dir = './rawdata/'
-ques_file = 'train_questions.json'
-imgid_file = 'train_images.json'
-choice_file = 'train_choices.json'
+ques_file = 'train_questions'
+choice_file = 'train_choices'
 cap_file = 'train_captions.json'
 wordvec_file = 'glove_300d.csv'
 
-with open(base_dir + ques_file) as file:
-    ques_dict = json.load(file)
+st = datetime.now()
+train_data = load_train_data(ques_file, choice_file, cap_file, base_dir)
+print('Done loading data. Using %s.' % str(datetime.now() - st))
 
-with open(base_dir + choice_file) as file:
-    choices_dict = json.load(file)
-
-with open(base_dir + cap_file) as file:
-    caps_dict = json.load(file)
-
-wordvec = pd.read_csv(base_dir + wordvec_file, sep=' ', header=None, index_col=0)
-dictionary = wordvec.index.values
-
-def clean_words(sentence, dictionary):
-    result = []
-    for word in sentence.split(' '):
-        result += judge(word, dictionary)
-
-    return result
-
-# vectorize
-ques_vec = {}
-for key, val in ques_dict.items():
-    word_bag = clean_words(val, dictionary)
-
-    if len(word_bag) == 0:
-        vec = np.zeros(wordvec.shape[1])
-    else:
-        vec = wordvec.loc[word_bag].fillna(0).values.mean(axis=0)
-
-    ques_vec[key] = vec
-
-choices_vec = defaultdict(list)
-for key, choices in choices_dict.items():
-    for val in choices:
-        word_bag = clean_words(val, dictionary)
-
-        if len(word_bag) == 0:
-            vec = np.zeros(wordvec.shape[1])
-        else:
-            vec = wordvec.loc[word_bag].fillna(0).values.mean(axis=0)
-
-        choices_vec[key].append(vec)
-
-caps_vec = defaultdict(list)
-for key, captions in caps_dict.items():
-    for val in captions:
-        word_bag = clean_words(val, dictionary)
-
-        if len(word_bag) == 0:
-            vec = np.zeros(wordvec.shape[1])
-        else:
-            vec = wordvec.loc[word_bag].fillna(0).values.mean(axis=0)
-
-        vec = wordvec.loc[word_bag].fillna(0).values.mean(axis=0)
-        caps_vec[key].append(vec)
-
+train_data = clean_data(train_data, wordvec_file, base_dir)
+print('Done transforming data. Using %s.' % str(datetime.now() - st))
 # make array
-
+wordvec = pd.read_csv(base_dir + wordvec_file, sep=' ', header=None, index_col=0)
+#vectorize(word_bag, wordvec)
 
 # construct model
 
--- utils.py
+++ utils.py
@@ -2,51 +2,141 @@
 # @Author: aaronlai
 # @Date:   2016-11-11 18:10:00
 # @Last Modified by:   AaronLai
-# @Last Modified time: 2016-11-11 21:58:06
+# @Last Modified time: 2016-11-12 00:55:05
+
+import pandas as pd
+import json
+import re
+
+from collections import defaultdict
 
 
 def judge(word, dictionary, cache=[]):
+    """check if the word in the dictionary"""
     word = word.strip('?/\\",:\'().*#_!`')
     word = word.lower()
 
     if len(word) == 0:
         pass
 
-    elif word in dictionary:
+    elif dictionary[word] == 1:
         cache.append(word)
 
     elif word[-2:] == "'s":
-        if word[:-2] in dictionary:
+        if dictionary[word[:-2]] == 1:
             cache.append(word[:-2])
             cache.append("'s")
 
     elif word[-3:] == "n't":
-        if word[:-3] in dictionary:
+        if dictionary[word[:-3]] == 1:
             cache.append(word[:-3])
             cache.append("n't")
 
     elif "/" in word:
         for vocab in word.split('/'):
-            if len(vocab) != 0 and vocab in dictionary:
+            if len(vocab) != 0 and dictionary[vocab] == 1:
                 cache.append(vocab)
 
     elif "-" in word:
         for vocab in word.split('-'):
-            if len(vocab) != 0 and vocab in dictionary:
+            if len(vocab) != 0 and dictionary[vocab] == 1:
                 cache.append(vocab)
 
     elif "," in word:
         for vocab in word.split(','):
-            if len(vocab) != 0 and vocab in dictionary:
+            if len(vocab) != 0 and dictionary[vocab] == 1:
                 cache.append(vocab)
 
     elif "(" in word:
         word = word[:word.index("(")]
-        if word in dictionary:
+        if dictionary[word] == 1:
             cache.append(word)
 
     elif word[-1] == "s":
-        if word[:-1] in dictionary:
+        if dictionary[word[:-1]] == 1:
             cache.append(word[:-1])
 
     return cache
+
+
+def clean_words(sentence, dictionary, result):
+    """transfrom the sentence or words into a list of words"""
+    for word in sentence.split(' '):
+        result = judge(word, dictionary, result)
+
+    return result
+
+
+def load_train_data(ques_file, choice_file, cap_file, base_dir='./rawdata/'):
+    """loading training data as a dictionary"""
+    train_ques = pd.read_csv(base_dir + ques_file, sep='\t')
+    train_choices = pd.read_csv(base_dir + choice_file, sep='\t').set_index('q_id')
+
+    with open(base_dir + cap_file) as file:
+        caps_dict = json.load(file)
+
+    train_data = defaultdict(dict)
+    for img_id, qid, ques in train_ques.values:
+        train_data[qid]['question'] = ques
+        train_data[qid]['caption'] = caps_dict[str(img_id)][0]
+
+        choices = train_choices.loc[qid]['choices']
+        clean_choices = []
+        for choice in re.split('\(.?\)', choices):
+            if len(choice.strip('" ')) > 0:
+                clean_choices.append(choice.strip('" '))
+
+        if len(clean_choices) != 5:
+            print(clean_choices)
+        train_data[qid]['choices'] = clean_choices
+
+    return train_data
+
+
+def clean_data(data, wordvec_file, base_dir='./rawdata'):
+    """trainform data into words in dictionary"""
+    wordvec = pd.read_csv(base_dir + wordvec_file, sep=' ', header=None, index_col=0)
+    dictionary = defaultdict(int)
+    for w in wordvec.index.values:
+        dictionary[w] = 1
+
+    result_data = {}
+    N = 0
+    for qid, data_dict in data.items():
+        result_dict = {}
+
+        # vectorize question
+        ques_vec = clean_words(data_dict['question'], dictionary, [])
+        result_dict['question'] = ques_vec
+
+        # vectorize choices
+        choices_vec = []
+        for choice in data_dict['choices']:
+            choi_vec = clean_words(choice, dictionary, [])
+            choices_vec.append(choi_vec)
+
+        result_dict['choices'] = choices_vec 
+
+        # vectorize caption
+        caption_vec = clean_words(data_dict['caption'], dictionary, [])
+        result_dict['caption'] = caption_vec
+
+        result_data[qid] = result_dict
+
+        N += 1
+        if N % 50 == 49:
+            print(N)
+
+    return result_data
+
+
+def vectorize(word_bag, wordvec):
+    """transfrom a text into a vector representation"""
+    if len(word_bag) == 0:
+        vec = np.zeros(wordvec.shape[1])
+    elif len(word_bag) == 1:
+        vec = wordvec.loc[word_bag[0]].values
+    else:
+        vec = wordvec.loc[word_bag].values.mean(axis=0)
+
+    return vec
